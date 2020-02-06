ddf580946eb1971f5361dbd5f5b55bb4b040e3a0
Vojtech Kolencik <vojtech.kolencik@vistaprint.com>
2020-02-06 13:49:21 +0100

    Add demo class from article

---
+++ xunit-output/Tests/ComponentShould.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests
+{
+    public class ComponentShould
+    {
+        private ITestOutputHelper _output;
+
+        public ComponentShould(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+        
+        [Fact]
+        public void DoStuff_WhenAskedTo()
+        {
+            _output.WriteLine("Output from test...");
+        }
+    }
+}
