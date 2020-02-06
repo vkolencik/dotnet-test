7203ada6d7bdd1052aaf395d6ed7add6a8d1612f
Vojtech Kolencik <vojtech.kolencik@vistaprint.com>
2020-02-06 01:48:32 +0100

    Reword

--- xunit-output/Tests/SortingHatShould.cs
+++ xunit-output/Tests/SortingHatShould.cs
@@ -24,13 +24,13 @@ namespace Tests
             var draco = new Wizard("Draco Malfoy");
             var wizards = new[] {draco};
             
-            _output.WriteLine("Sorting wizards:");
+            _output.WriteLine("Wizards to be sorted:");
             Array.ForEach(wizards, w => _output.WriteLine($"* {w}"));
 
             // act
             var houseMemberships = sortingHat.Sort(wizards);
             
-            _output.WriteLine("Result house memberships:");
+            _output.WriteLine("Assigned house memberships:");
             houseMemberships.ToList().ForEach(kvp => _output.WriteLine($"* {kvp.Key} in {kvp.Value}"));
 
             _output.WriteLine(JsonConvert.SerializeObject(houseMemberships));
